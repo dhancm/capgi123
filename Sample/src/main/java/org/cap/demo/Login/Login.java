ccd8f26a136b4b24b63e1ccbd90a6d990adf4c79
dhancm <dhancm@DIN78000385.corp.capgemini.com>
2019-07-04 14:49:55 +0530

    login field included with login class

---
+++ Sample/src/main/java/org/cap/demo/Login/Login.java
@@ -0,0 +1,27 @@
+package org.cap.demo.Login;
+
+public class Login {
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Login [getUsername()=" + getUsername() + ", getPassword()=" + getPassword() + ", username=" + username
+				+ ", password=" + password + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+
+}
